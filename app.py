79cab2679dda7ea17f8f0b6203b9b6df8295cd8c
S v Akshay Kumar <152531143+SvAkshayKumar@users.noreply.github.com>
2024-05-09 11:14:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, jsonify,flash
+from flask import Flask, render_template, request, redirect, jsonify
 import sqlite3
 app = Flask(__name__)
 def connect_db():
